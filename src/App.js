1a76757531a555dac362bd854358697d82fec6dd
vladimir.simanek <vladimir.simanek@gmail.com>
2018-08-20 21:16:39 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './scss/App.scss';
+
+class App extends Component {
+
+    render() {
+        return(
+            <h1>This is CALCULATOR!!!</h1>
+        );
+    };
+
+}
+
+export default App;
